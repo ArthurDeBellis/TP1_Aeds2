9e5804c872bd4de6d8c39c5208600f147243d248
ArthurDeBellis <arthur.bellis@ufv.br>
2019-04-16 18:43:33 -0300

    Índice inverso.h

--- Libs/IndInverso.h
+++ Libs/IndInverso.h
@@ -1,12 +1,30 @@
 #ifndef INDINVERSO_H
 #define INDINVERSO_H
 
-typedef struct IndInverso *ApIndInverso;
+typedef struct TCelula *ApIndInverso;
 
-typedef struct IndInverso{
+typedef struct TCelula{
   int qtde;
-  char* idDoc;
-  ApIndInverso Prox;
-}IndInverso;
+  int idDoc;
+  ApIndInverso pProx;
+}TCelula;
+
+typedef struct{
+  ApIndInverso pPrimeiro;
+  ApIndInverso pUltimo;
+}TLista;
+
+//Funções
+void IniciaLista(TLista *pLista);
+int ListaVazia(TLista Lista);
+
+void InserirNovo(TLista *Lista, int qtde, int idDoc);
+void InserirUltimo(TLista *Lista, int qtde, int idDoc);
+
+int RemoverLista(TLista *pLista, int Id);
+ApIndInverso ProcurarLista (TLista *pLista, int Id);
+
+void ImprimirLista(TLista Lista);
+
 
 #endif
