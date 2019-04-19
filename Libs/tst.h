08f970ceb02f416234a6fb9857b1d63c8c7564df
ArthurDeBellis <arthur.bellis@ufv.br>
2019-04-19 17:04:26 -0300

    Eu até tentei bastante, mas não sei fazer isso fncionar :(

--- Libs/tst.h
+++ Libs/tst.h
@@ -1,23 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <malloc.h>
+
 #ifndef TST_H
 #define TST_H
 
-
-enum ntipo {NAO,SIM}; // não entendi isso aqui, mas presumi q era o marcador de fim de string
+typedef enum ntipo {
+  NAO,SIM
+}ntipo;
 
 typedef struct NoTST {
   char letra;
   struct NoTST* Esq;
   struct NoTST* Dir;
   struct NoTST* Meio;
-  enum ntipo Fim;
+  ntipo Fim;
 }NoTST;
 
 NoTST* RAIZ;
 
-NoTST* CriarNoTST(char letra, enum ntipo t);
+NoTST* CriarNoTST(char letra, ntipo t);
 NoTST* InserirNoTST(const char* palavra, NoTST* No);
-NoTST* BuscarNoTST(const char* palavra);
-
-void ImprimirMesmoPrefixo(const char* prefixo);
+NoTST* BuscarNoTST(NoTST* arvore, const char* palavra);
+void BuscaProfunda(const char* prefixo, NoTST* inicio);
+void ImprimirMesmoPrefixo(NoTST* arvore, const char* prefixo);
 
 #endif
+
